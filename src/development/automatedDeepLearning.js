49e2ecd2e33567c26157e965e31fb3a43bd0c09f
Adeniyi Janet <adeniyijanet61@gmail.com>
2023-07-29 16:39:34 +0100

    fixed paren_id and block_id

--- src/development/automatedDeepLearning.js
+++ src/development/automatedDeepLearning.js
@@ -10,28 +10,9 @@ const { validateRequiredParameters } = require("../helpers/validation");
  */
 const AutomatedDeepLearning = (superclass) =>
   class extends superclass {
-    /**
-     * Data Input
-     *
-     * Specify the data sources, this functionality can take database connection, CSV or JSON files
-     *
-     * @param {int} project_id current project's ID
-     * @param {null} parent_id previous block ID
-     * @param {int} block_id current block ID
-     * @param {object} args block arguments (refer to documentation)
-     *
-     * {@link https://autogon-ai.gitbook.io/autogon-for-developers/autogon-engine/data-processing/data-input-dp_1}
-     *
-     * @returns {object} StateManagement Object
-     */
     auto_image_classifier_init(project_id, parent_id, block_id, args = {}) {
       // mandatory parameters
       const functionCode = "A_DL_IMC_I";
-
-      // these parameters are compulsory for this block
-      parent_id = null;
-      block_id = 1;
-
       // parameter validation
       validateRequiredParameters(functionCode, args);
 
@@ -47,51 +28,6 @@ const AutomatedDeepLearning = (superclass) =>
     auto_image_classifier_train(project_id, parent_id, block_id, args = {}) {
       // mandatory parameters
       const functionCode = "A_DL_IMC_T";
-
-      // these parameters are compulsory for this block
-      parent_id = null;
-      block_id = 1;
-
-      // parameter validation
-      validateRequiredParameters(functionCode, args);
-
-      // send request
-      return this.sendRequest("POST", constants.ENGINE_ENDPOINT, {
-        project_id,
-        parent_id,
-        block_id,
-        function_code: functionCode,
-        args,
-      });
-    }
-    auto_image_classifier_evaluate(project_id, parent_id, block_id, args = {}) {
-      // mandatory parameters
-      const functionCode = "A_DL_IMC_E";
-
-      // these parameters are compulsory for this block
-      parent_id = null;
-      block_id = 1;
-
-      // parameter validation
-      validateRequiredParameters(functionCode, args);
-
-      // send request
-      return this.sendRequest("POST", constants.ENGINE_ENDPOINT, {
-        project_id,
-        parent_id,
-        block_id,
-        function_code: functionCode,
-        args,
-      });
-    }
-    auto_image_classifier_predict(project_id, parent_id, block_id, args = {}) {
-      // mandatory parameters
-      const functionCode = "A_DL_IMC_P";
-
-      // these parameters are compulsory for this block
-      parent_id = null;
-      block_id = 1;
-
       // parameter validation
       validateRequiredParameters(functionCode, args);
 
@@ -107,11 +43,6 @@ const AutomatedDeepLearning = (superclass) =>
     auto_image_regressor_init(project_id, parent_id, block_id, args = {}) {
       // mandatory parameters
       const functionCode = "A_DL_IMR";
-
-      // these parameters are compulsory for this block
-      parent_id = null;
-      block_id = 1;
-
       // parameter validation
       validateRequiredParameters(functionCode, args);
 
@@ -127,51 +58,6 @@ const AutomatedDeepLearning = (superclass) =>
     auto_image_regressor_train(project_id, parent_id, block_id, args = {}) {
       // mandatory parameters
       const functionCode = "A_DL_IMR_T";
-
-      // these parameters are compulsory for this block
-      parent_id = null;
-      block_id = 1;
-
-      // parameter validation
-      validateRequiredParameters(functionCode, args);
-
-      // send request
-      return this.sendRequest("POST", constants.ENGINE_ENDPOINT, {
-        project_id,
-        parent_id,
-        block_id,
-        function_code: functionCode,
-        args,
-      });
-    }
-    auto_image_regressor_evaluate(project_id, parent_id, block_id, args = {}) {
-      // mandatory parameters
-      const functionCode = "A_DL_IMR_E";
-
-      // these parameters are compulsory for this block
-      parent_id = null;
-      block_id = 1;
-
-      // parameter validation
-      validateRequiredParameters(functionCode, args);
-
-      // send request
-      return this.sendRequest("POST", constants.ENGINE_ENDPOINT, {
-        project_id,
-        parent_id,
-        block_id,
-        function_code: functionCode,
-        args,
-      });
-    }
-    auto_image_regressor_predict(project_id, parent_id, block_id, args = {}) {
-      // mandatory parameters
-      const functionCode = "A_DL_IMR_P";
-
-      // these parameters are compulsory for this block
-      parent_id = null;
-      block_id = 1;
-
       // parameter validation
       validateRequiredParameters(functionCode, args);
 
@@ -187,11 +73,6 @@ const AutomatedDeepLearning = (superclass) =>
     auto_text_classifier_init(project_id, parent_id, block_id, args = {}) {
       // mandatory parameters
       const functionCode = "A_DL_TXC_I";
-
-      // these parameters are compulsory for this block
-      parent_id = null;
-      block_id = 1;
-
       // parameter validation
       validateRequiredParameters(functionCode, args);
 
@@ -208,50 +89,6 @@ const AutomatedDeepLearning = (superclass) =>
       // mandatory parameters
       const functionCode = "A_DL_TXC_T";
 
-      // these parameters are compulsory for this block
-      parent_id = null;
-      block_id = 1;
-
-      // parameter validation
-      validateRequiredParameters(functionCode, args);
-
-      // send request
-      return this.sendRequest("POST", constants.ENGINE_ENDPOINT, {
-        project_id,
-        parent_id,
-        block_id,
-        function_code: functionCode,
-        args,
-      });
-    }
-    auto_text_classifier_evaluate(project_id, parent_id, block_id, args = {}) {
-      // mandatory parameters
-      const functionCode = "A_DL_TXC_E";
-
-      // these parameters are compulsory for this block
-      parent_id = null;
-      block_id = 1;
-
-      // parameter validation
-      validateRequiredParameters(functionCode, args);
-
-      // send request
-      return this.sendRequest("POST", constants.ENGINE_ENDPOINT, {
-        project_id,
-        parent_id,
-        block_id,
-        function_code: functionCode,
-        args,
-      });
-    }
-    auto_text_classifier_predict(project_id, parent_id, block_id, args = {}) {
-      // mandatory parameters
-      const functionCode = "A_DL_TXC_P";
-
-      // these parameters are compulsory for this block
-      parent_id = null;
-      block_id = 1;
-
       // parameter validation
       validateRequiredParameters(functionCode, args);
 
@@ -267,11 +104,6 @@ const AutomatedDeepLearning = (superclass) =>
     auto_text_regressor_init(project_id, parent_id, block_id, args = {}) {
       // mandatory parameters
       const functionCode = "A_DL_TXR_I";
-
-      // these parameters are compulsory for this block
-      parent_id = null;
-      block_id = 1;
-
       // parameter validation
       validateRequiredParameters(functionCode, args);
 
@@ -287,51 +119,6 @@ const AutomatedDeepLearning = (superclass) =>
     auto_text_regressor_train(project_id, parent_id, block_id, args = {}) {
       // mandatory parameters
       const functionCode = "A_DL_TXR_T";
-
-      // these parameters are compulsory for this block
-      parent_id = null;
-      block_id = 1;
-
-      // parameter validation
-      validateRequiredParameters(functionCode, args);
-
-      // send request
-      return this.sendRequest("POST", constants.ENGINE_ENDPOINT, {
-        project_id,
-        parent_id,
-        block_id,
-        function_code: functionCode,
-        args,
-      });
-    }
-    auto_text_regressor_evaluate(project_id, parent_id, block_id, args = {}) {
-      // mandatory parameters
-      const functionCode = "A_DL_TXR_E";
-
-      // these parameters are compulsory for this block
-      parent_id = null;
-      block_id = 1;
-
-      // parameter validation
-      validateRequiredParameters(functionCode, args);
-
-      // send request
-      return this.sendRequest("POST", constants.ENGINE_ENDPOINT, {
-        project_id,
-        parent_id,
-        block_id,
-        function_code: functionCode,
-        args,
-      });
-    }
-    auto_text_regressor_predict(project_id, parent_id, block_id, args = {}) {
-      // mandatory parameters
-      const functionCode = "A_DL_TXR_P";
-
-      // these parameters are compulsory for this block
-      parent_id = null;
-      block_id = 1;
-
       // parameter validation
       validateRequiredParameters(functionCode, args);
 
@@ -352,10 +139,6 @@ const AutomatedDeepLearning = (superclass) =>
     ) {
       // mandatory parameters
       const functionCode = "A_DL_SDC_I";
-
-      // these parameters are compulsory for this block
-      parent_id = null;
-      block_id = 1;
 
       // parameter validation
       validateRequiredParameters(functionCode, args);
@@ -378,60 +161,6 @@ const AutomatedDeepLearning = (superclass) =>
       // mandatory parameters
       const functionCode = "A_DL_SDC_T";
 
-      // these parameters are compulsory for this block
-      parent_id = null;
-      block_id = 1;
-
-      // parameter validation
-      validateRequiredParameters(functionCode, args);
-
-      // send request
-      return this.sendRequest("POST", constants.ENGINE_ENDPOINT, {
-        project_id,
-        parent_id,
-        block_id,
-        function_code: functionCode,
-        args,
-      });
-    }
-    auto_structured_data_classifier_evaluate(
-      project_id,
-      parent_id,
-      block_id,
-      args = {}
-    ) {
-      // mandatory parameters
-      const functionCode = "A_DL_SDC_E";
-
-      // these parameters are compulsory for this block
-      parent_id = null;
-      block_id = 1;
-
-      // parameter validation
-      validateRequiredParameters(functionCode, args);
-
-      // send request
-      return this.sendRequest("POST", constants.ENGINE_ENDPOINT, {
-        project_id,
-        parent_id,
-        block_id,
-        function_code: functionCode,
-        args,
-      });
-    }
-    auto_structured_data_classifier_predict(
-      project_id,
-      parent_id,
-      block_id,
-      args = {}
-    ) {
-      // mandatory parameters
-      const functionCode = "A_DL_SDC_P";
-
-      // these parameters are compulsory for this block
-      parent_id = null;
-      block_id = 1;
-
       // parameter validation
       validateRequiredParameters(functionCode, args);
 
@@ -453,10 +182,6 @@ const AutomatedDeepLearning = (superclass) =>
       // mandatory parameters
       const functionCode = "A_DL_SDR_I";
 
-      // these parameters are compulsory for this block
-      parent_id = null;
-      block_id = 1;
-
       // parameter validation
       validateRequiredParameters(functionCode, args);
 
@@ -477,60 +202,6 @@ const AutomatedDeepLearning = (superclass) =>
     ) {
       // mandatory parameters
       const functionCode = "A_DL_SDR_T";
-
-      // these parameters are compulsory for this block
-      parent_id = null;
-      block_id = 1;
-
-      // parameter validation
-      validateRequiredParameters(functionCode, args);
-
-      // send request
-      return this.sendRequest("POST", constants.ENGINE_ENDPOINT, {
-        project_id,
-        parent_id,
-        block_id,
-        function_code: functionCode,
-        args,
-      });
-    }
-    auto_structured_data_regressor_evaluate(
-      project_id,
-      parent_id,
-      block_id,
-      args = {}
-    ) {
-      // mandatory parameters
-      const functionCode = "A_DL_SDR_E";
-
-      // these parameters are compulsory for this block
-      parent_id = null;
-      block_id = 1;
-
-      // parameter validation
-      validateRequiredParameters(functionCode, args);
-
-      // send request
-      return this.sendRequest("POST", constants.ENGINE_ENDPOINT, {
-        project_id,
-        parent_id,
-        block_id,
-        function_code: functionCode,
-        args,
-      });
-    }
-    auto_structured_data_regressor_predict(
-      project_id,
-      parent_id,
-      block_id,
-      args = {}
-    ) {
-      // mandatory parameters
-      const functionCode = "A_DL_SDR_P";
-
-      // these parameters are compulsory for this block
-      parent_id = null;
-      block_id = 1;
 
       // parameter validation
       validateRequiredParameters(functionCode, args);
